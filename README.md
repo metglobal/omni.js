b6545c905bd3be635dba0d6679b6ab1743df46ea
Fatih Kadir Akın <fatih.akin@metglobal.com>
2014-01-10 17:29:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Adobe Omniture Analytics Client
+
+Adobe Omniture Client is a client for websites which uses Adobe Analytics.
+
+## Overview
+
+It has a clean interface than Adobe's official client.
+
+It simplifies the usage of API which detailed in [documentation](https://microsite.omniture.com/t2/help/en_US/sc/implement/index.html)
+
+```javascript
+// Easily create an instance
+omni("namespace")
+
+  // Set Variables
+  .set("pageName", "My Main Site")
+  
+  // Define Routes
+  .route(/products.html$/, function () {
+    this.set("pageName", "Products Page")
+        .evar(1, "User Entered to Products Page")
+  })
+
+  .route(/purchase\/([0-9]+)/, function (purchaseId) {
+    
+    this.set("purchaseID", purchaseId);
+
+    // Use Preset Methods for eCommerce
+    this.event.products([
+      {category: "Running", product: "Shoe", quantity: 1, price: 69.95},
+      {category: "Running", product: "Socks", quantity: 1, price: 29.95}
+    ]);
+  })
+  
+  // Track custom events from DOM
+  .track("button.purchase", "click", /*evar:*/15, "Purchase", "Purchase Button Clicked");
+  .run();
+```
+
+### Resources
+[https://microsite.omniture.com/t2/help/en_US/sc/implement/index.html](https://microsite.omniture.com/t2/help/en_US/sc/implement/index.html)
+
+### License
+
+Copyright (c) 2013 Metglobal LLC.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ tests/omni_test.js
@@ -0,0 +1,202 @@
+var AppMeasurement, Visitor;
+AppMeasurement = {
+  getInstance: function (instance) {
+    return {
+      instance: instance,
+      t: function () {
+        AppMeasurement.called_t = true;
+      },
+      tl: function () {
+        AppMeasurement.called_tl = true;
+      },
+      util: function () {}
+    };
+  }
+};
+
+Visitor = function () {}
+Visitor.getInstance = function (instance) { return instance; }
+
+test("Test Construction", function() {
+  
+  var omniTest = omni("hello");
+
+  ok(omniTest.instance.visitorNamespace == "helloVisitor",
+    "visitorNamespace of hello is helloVisitor");
+
+  ok(omniTest.instance.account == "hello",
+    "visitor account is hello");
+
+  ok(omniTest.instance.charSet == "UTF-8",
+    "Charset is UTF-8");
+
+  ok(typeof omniTest.routes == "object" && omniTest.routes.length == 0,
+    "Omni routes is set and empty");
+
+});
+
+test("Test setOption", function () {
+  
+  var omniTest = omni("hello"); 
+
+  omniTest.setOption("hello", "world");
+  
+  ok(omniTest.instance.hello == "world",
+    "setOption sets instance variable");
+
+  ok(omniTest.hello !== "hello",
+    "setOption doesn't touch omni itself");
+
+});
+
+test("Test getOption", function () {
+  
+  var omniTest = omni("hello");
+
+  omniTest.setOption("hello", "world");
+  ok(omniTest.getOption("hello") == "world",
+    "Setting hello and getting back is ok.");
+
+});
+
+test("Test setSeqParam", function () {
+  
+  var omniTest = omni("hello");
+  omniTest.setSeqParam("hello", 1, "world");
+  omniTest.setSeqParam("world", 4, "hello");
+
+  ok(omniTest.instance.hello1 == "world",
+    "Setting hello with seq 1 sets hello1.");
+  
+  ok(omniTest.instance.world4 == "hello",
+  "Setting world with seq 4 sets world.");
+
+});
+
+test("Set visitor option", function () {
+  
+  var omniTest = omni("hello");
+  omniTest.setVisitorOption("hello", "world");
+
+  ok(omniTest.visitor.hello == "world",
+    "Setting visitor option hello sets value to world");
+
+});
+
+test("Set EVar", function () {
+  
+  var omniTest = omni("hello");
+  omniTest.setEVar(1, "world");
+  omniTest.setEVar(5, "hello");
+
+  ok(omniTest.instance.eVar1 == "world",
+    "evar1 is world");
+
+  ok(omniTest.instance.eVar5 == "hello",
+    "evar5 is hello");
+
+});
+
+test("Set Hier", function () {
+  
+  var omniTest = omni("hello");
+  omniTest.setHier(1, "world");
+  omniTest.setHier(5, "hello");
+
+  ok(omniTest.instance.hier1 == "world",
+    "hier1 is world");
+
+  ok(omniTest.instance.hier5 == "hello",
+    "hier5 is hello");
+
+});
+
+test("Set Prop", function () {
+
+  var omniTest = omni("hello");
+  omniTest.setProp(1, "world");
+  omniTest.setProp(5, "hello");
+
+  ok(omniTest.instance.prop1 == "world",
+    "prop1 is world");
+
+  ok(omniTest.instance.prop5 == "hello",
+    "prop1 is hello");
+});
+
+
+test("add a string route", function () {
+
+  var omniTest = omni("hello");
+  var spy = function () {};
+  omniTest.route("world", spy);
+
+  ok(omniTest.routes.length == 1,
+    "adding route increases routes length");
+
+  ok(omniTest.routes[0].callback == spy,
+    "the callback is the spy");
+
+  ok(omniTest.routes[0].route == "world",
+    "the route is world");
+
+});
+
+test("add a RegExp route", function () {
+
+  var omniTest = omni("hello");
+  var spy = function () {};
+  omniTest.route(/hello/, spy);
+
+  ok(omniTest.routes.length == 1,
+    "adding route increases routes length");
+
+  ok(omniTest.routes[0].callback == spy,
+    "the callback is the spy");
+
+  ok(omniTest.routes[0].route instanceof RegExp,
+    "the route is regexp");
+
+  ok(omniTest.routes[0].route.test("hello"),
+    "the route matches the hello string");
+
+});
+
+test("test aliases", function () {
+
+  var omniTest = omni("hello");
+
+  ok(omniTest.set == omniTest.setOption, "set -> setOption");
+  ok(omniTest.get == omniTest.getOption, "get -> getOption");
+  ok(omniTest.evar == omniTest.setEVar, "evar -> setEVar");
+  ok(omniTest.hier == omniTest.setHier, "set -> setHier");
+  ok(omniTest.prop == omniTest.setProp, "set -> setProp");
+
+});
+
+test("test run", function () {
+
+  var omniTest = omni("hello");
+  var called = false, called2 = false, called3 = false;
+  var spy = function () { called = true; };
+  var spy2 = function () { called2 = true; };
+  var spy3 = function () { called3 = true; };
+  omniTest.route("hello", spy);
+  omniTest.route("world", spy2);
+  omniTest.route(/h.o/, spy3);
+
+  ok(called == false && called2 == false && called3 == false,
+    "adding routes doesnt call spies.");
+
+  omniTest.run("hello");
+  ok(called == true && called2 == false && called3 == false,
+    "run hello route spy");
+
+  omniTest.run("world");
+  ok(called == true && called2 == true && called3 == false,
+    "run world route spy");
+  
+  omniTest.run("hio");
+  ok(called == true && called2 == true && called3 == true,
+    "run hio route spy with regexp");
+});
